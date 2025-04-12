b2eb26f9a47df62a49312db291a262d784e14021
tatsuya.kitabori <45zombie45@gmail.com>
2025-04-12 09:16:35 +0900

    install and adapt vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remix({
-    ignoredRouteFiles: ["**/*.css"],
-    future: {
-      unstable_optimizeDeps: true,
-    },
-  })],
+  plugins: [
+    remix({
+        ignoredRouteFiles: ["**/*.css"],
+        future: {
+          unstable_optimizeDeps: true,
+        },
+      }),
+    tsconfigPaths()
+  ],
 });
