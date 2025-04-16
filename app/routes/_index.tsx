00f9d0c4baa7f45a693368b486b679a2e065a70d
TatsuyaKITABORI <40175430+bo-ri@users.noreply.github.com>
2025-04-17 08:49:47 +0900

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import type { MetaFunction } from 'react-router';
-import { Link as RemixLink } from 'react-router';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import * as React from "react";
+import type { MetaFunction } from "react-router";
+import { Link as RemixLink, useFetcher } from "react-router";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 // https://remix.run/docs/en/main/route/meta
 export const meta: MetaFunction = () => [
-  { title: 'Remix Starter' },
-  { name: 'description', content: 'Welcome to remix!' },
+  { title: "Remix Starter" },
+  { name: "description", content: "Welcome to remix!" },
 ];
 
 // https://remix.run/docs/en/main/file-conventions/routes#basic-routes
 export default function Index() {
+  const fetcher = useFetcher();
   return (
     <React.Fragment>
       <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
@@ -20,6 +21,10 @@ export default function Index() {
       <Link to="/about" color="secondary" component={RemixLink}>
         Go to the about page
       </Link>
+      <fetcher.Form method="POST" action="/action">
+        <input type="text" name="input" />
+        <button type="submit">test</button>
+      </fetcher.Form>
     </React.Fragment>
   );
 }
